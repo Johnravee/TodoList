03f114813c75a592c2fb53d92e84d32216d92622
Johnrave <johnravemimayn@gmail.com>
2023-02-20 21:06:06 -0800

    Done

--- script.js
+++ script.js
@@ -1,21 +1,67 @@
 
-
+//*
 const addbtn = document.querySelector(".addbtn")
+
+//! Adding input value 
 addbtn.addEventListener('click', ()=>{
     const input = document.querySelector("#input")
-    const note = document.querySelector(".list")
+  
+    //*If input have value 
+    if(input.value.length > 1)
+    {
+        //*display
+        const note = document.querySelector(".list")
     
-    //*Creating a list 
-    let todo = `<li>${input.value}<button class="remove">X</button</li>`
-    note.insertAdjacentHTML("afterbegin", todo)
+        //*Creating a list 
+        let todo = `<li><b>${input.value}</b><button class="remove">X</button</li>`
+        
+        //*inserting a value to display
+        note.insertAdjacentHTML("afterbegin", todo)
 
-    let remov = document.querySelector(".remove")
+        //*Remove list when click
+        let remov = document.querySelector(".remove")
+        remov.addEventListener('click', ()=>{
+            remov.parentElement.remove()
+        })
 
-    remov.addEventListener('click', ()=>{
-        remov.parentElement.remove()
-    })
 
+    }
+    //*If input don't have value 
+    else
+    {
+        //*Pop up
+       let warning = prompt("No value receive, Please input a value")
+       
+       //*inserting Pop up value
+       if(warning.length > 1 )
+       {
+         
+        const note = document.querySelector(".list")
+    
+        //*Creating a list 
+        let todo = `<li><b>${warning}</b><button class="remove">X</button</li>`
+        note.insertAdjacentHTML("afterbegin", todo)
+    
+        
+        let remov = document.querySelector(".remove")
+        remov.addEventListener('click', ()=>{
+            remov.parentElement.remove()
+        })
+       }
+    }
 
 })
+
+//!remove all list in the container
+const removAll = () =>{
+    const note = document.querySelector(".list")
+      //Remove the element
+    note.remove()
+
+    //reload the page to reset
+    location.reload()
+
+}
+
 
 
